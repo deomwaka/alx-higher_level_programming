cb4d78ae9bc7c9e3aa9981965a300e5d0d9ac9f6
deomwaka <deogratiusmwaka@gmail.com>
2023-06-16 13:52:16 -0700

    print basic info about Python lists and bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,17 +1,16 @@
 #include <Python.h>
+
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
 /**
-* print_python_list - Prints basic info about Python lists.
-* @p: A PyObject list object.
-*/
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
 void print_python_list(PyObject *p)
 {
 	int size, alloc, i;
-
 	const char *type;
-
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
 
@@ -21,6 +20,7 @@ void print_python_list(PyObject *p)
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
+
 	for (i = 0; i < size; i++)
 	{
 		type = list->ob_item[i]->ob_type->tp_name;
@@ -29,30 +29,32 @@ void print_python_list(PyObject *p)
 			print_python_bytes(list->ob_item[i]);
 	}
 }
+
 /**
-* print_python_bytes - Prints basic info about Python byte objects.
-* @p: A PyObject byte object.
-*/
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
 void print_python_bytes(PyObject *p)
 {
 	unsigned char i, size;
-
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info\n");
-
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
-		printf(" [ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	printf(" size: %ld\n", ((PyVarObject *)p)->ob_size);
-	printf(" trying string: %s\n", bytes->ob_sval);
+
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
 	if (((PyVarObject *)p)->ob_size > 10)
 		size = 10;
 	else
 		size = ((PyVarObject *)p)->ob_size + 1;
-	printf(" first %d bytes: ", size);
+
+	printf("  first %d bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
 		printf("%02hhx", bytes->ob_sval[i]);
@@ -62,3 +64,4 @@ void print_python_bytes(PyObject *p)
 			printf(" ");
 	}
 }
+
