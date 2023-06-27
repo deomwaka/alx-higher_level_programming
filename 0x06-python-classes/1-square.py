155e37734a6fe4810ca0eb965bf469154baf7204
deomwaka <deogratiusmwaka@gmail.com>
2023-06-27 12:32:21 -0700

    class Square with a private attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+""" Define a class Square """
+
+
+class Square:
+    """ Instatiate attribute size """
+    def __init__(self, size):
+        """ Privatize size """
+        self.__size = size
