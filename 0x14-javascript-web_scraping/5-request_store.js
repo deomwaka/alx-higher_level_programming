45003d1d934324add15589e104a76657ccb6eadf
deomwaka <58949829+deomwaka@users.noreply.github.com>
2023-10-25 00:27:04 +0300

    Delete 0x14-javascript-web_scraping directory
    Removed directory

--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const req = require('request');
-const fs = require('fs');
-const url = process.argv[2];
-const Fname = process.argv[3];
-req
-  .get(url)
-  .pipe(fs.createWriteStream(Fname));
